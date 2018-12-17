7e2b4180ce464c9dcee7a9a39f3c26865380a4e4
Augustine <augu@mailbox.com>
2018-12-17 00:04:03 +0000

    initial commit

---
+++ resources/views/layouts/sidebar_navmenu.blade.php
@@ -0,0 +1,35 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="{!! ('/home') !!}" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                  <i class="fa fa-th nav-icon"></i>
+                  <p>Management
+                    <i class="right fa fa-angle-right"></i>
+                  </p>
+                </a>
+              </li>
+             
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-user nav-icon"></i>
+              <p>Profile</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-power-off nav-icon"></i>
+              <p>Logout</p>
+            </a>
+          </li>
+        </ul>
